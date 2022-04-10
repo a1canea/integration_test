55c583031b341366f38ff9b42fe06b668366bbe3
Timur Alymbekov <a1canearisto@gmail.com>
2022-04-10 21:58:55 +0600

    fixing main.dart file path

--- integration_test/app_test.dart
+++ integration_test/app_test.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
-import 'package:test_project/resocoder_integration_test/main.dart';
+import 'package:test_project/main.dart';
 
 void main() {
   IntegrationTestWidgetsFlutterBinding.ensureInitialized();
